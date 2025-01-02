568841aceb1b22e7a2e5c8370b9ec552cb912dff
devandresrw <aroldan0@misena.edu.co>
2025-01-01 20:22:26 -0500

    Initial deploys2

--- src/components/containers/App.tsx
+++ src/components/containers/App.tsx
@@ -2,6 +2,7 @@ import ContainerIntro from '@/components/containers/IntroCo'
 export const App = () => {
   return (
     <div className="h-screen w-screen flex justify-center items-center">
+
       <ContainerIntro />
     </div>
   )
