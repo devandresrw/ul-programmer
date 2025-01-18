a9628a0d9fc6e0dfb7afa3d74745e5cd7093f6e3
devandresrw <aroldan0@misena.edu.co>
2025-01-18 18:13:21 -0500

    Escritorio 36%

--- src/components/3d/models/ContainersModel.tsx
+++ src/components/3d/models/ContainersModel.tsx
@@ -4,7 +4,7 @@ export const ContainersModel = () => {
 
   return (
     <group>
-        <TorusKnots numx={2} numy={3} numz={1} p={2} position={[1, 1, 1]} q={1} radialSegments={2} radius={4} rotation={[1, 1, 1]} tube={2} tubularSegments={2} key={6} />
+        <TorusKnots />
     </group>
   )
 }
