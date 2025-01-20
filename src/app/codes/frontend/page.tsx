e235aa34be0163efea94c6b9668223910475dc5b
devandresrw <aroldan0@misena.edu.co>
2025-01-20 16:57:44 -0500

    Codes PageFrontend

--- src/app/codes/frontend/page.tsx
+++ src/app/codes/frontend/page.tsx
@@ -4,10 +4,30 @@ export default function FrontendPage() {
     <div className="">
   
 <ASCIIText
-  text='hello_world'
+  text='front-end'
   enableWaves={true}
   asciiFontSize={8}
-/>
+  planeBaseHeight={9}
+  textFontSize={3}
+/>    
+      <div className="">
+        <div className="">
+          <p>Front-end</p>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>React</li>
+            <li>Next.js</li>
+            <li>Vue.js</li>
+            <li>Angular</li>
+          </ul>
+        </div>
+        <div className="">
+          <p>Descripción</p>
+          <p>El front-end es la parte de la aplicación que interactúa con los usuarios. Es la parte de la aplicación que los usuarios ven y con la que interactúan. El front-end se compone de tres partes: la estructura (HTML), la presentación (CSS) y la interactividad (JavaScript).</p>
+        </div>
+      </div>
     </div>
   )
 }
