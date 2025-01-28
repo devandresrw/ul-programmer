b8c5cad8f598a48e1e2896906a529131c3429dc9
devandresrw <aroldan0@misena.edu.co>
2025-01-28 11:00:37 -0500

    Newbie challengue

--- src/data/frontend.ts
+++ src/data/frontend.ts
@@ -16,7 +16,7 @@ export const frontendCards = [
       category: 'Newbie',
       image: '/ceo.jpg',
       title: 'Social links profile',
-      description: `Una página HTML básica que muestra enlaces a perfiles sociales
+      description: `Una página básica que muestra enlaces a perfiles sociales
        con un diseño limpio y disposición sencilla y responsiva.`,
       technologies: ['React', 'TypeScript', 'TailwindCSS', 'Next JS'],
       link: 'https://mentor-newbie-03-social-links-profile.vercel.app/',
@@ -27,10 +27,10 @@ export const frontendCards = [
       category: 'Newbie',
       image: '/ceo.jpg',
       title: 'Blog preview card',
-      description: `Una tarjeta HTML básica diseñada para mostrar vistas previas de blogs, 
+      description: `Una tarjeta básica diseñada para mostrar vistas previas de blogs, 
       incluyendo título, descripción breve e imagen destacada.`,
       technologies: ['React', 'TypeScript', 'TailwindCSS', 'Next JS'],
-      link: 'https://mentor-newbie-01-blog-preview-card.vercel.app/',
+      link: 'https://mentor-newbie-01-blog-preview-card.vercel.app/ ',
       color: ''
     },
     {
@@ -38,10 +38,10 @@ export const frontendCards = [
       category: 'Newbie',
       image: '/ceo.jpg',
       title: 'FAQ accordion',
-      description: `Una tarjeta HTML básica diseñada para mostrar vistas previas de blogs, 
-      incluyendo título, descripción breve e imagen destacada.`,
+      description: `Un diseño básico para una sección de preguntas frecuentes, 
+      con preguntas listadas y respuestas visibles al expandir manualmente.`,
       technologies: ['React', 'TypeScript', 'TailwindCSS', 'Next JS'],
-      link: 'https://mentor-newbie-01-blog-preview-card.vercel.app/',
+      link: 'https://mentor-newbie-04-faq-acordion.vercel.app/',
       color: ''
     },
     {
@@ -49,10 +49,10 @@ export const frontendCards = [
       category: 'Newbie',
       image: '/ceo.jpg',
       title: 'Workit landing page',
-      description: `Una tarjeta HTML básica diseñada para mostrar vistas previas de blogs, 
-      incluyendo título, descripción breve e imagen destacada.`,
+      description: `Landing básica para una aplicación de análisis de datos.
+       Destaca información procesable, decisiones basadas en datos y precios accesibles.`,
       technologies: ['React', 'TypeScript', 'TailwindCSS', 'Next JS'],
-      link: 'https://mentor-newbie-01-blog-preview-card.vercel.app/',
+      link: 'https://mentor-newbie-05-workit-landing-page-cki8.vercel.app/',
       color: ''
     },
     {
@@ -60,10 +60,10 @@ export const frontendCards = [
       category: 'Newbie',
       image: '/ceo.jpg',
       title: 'Results summary component',
-      description: `Una tarjeta HTML básica diseñada para mostrar vistas previas de blogs, 
-      incluyendo título, descripción breve e imagen destacada.`,
+      description: `Un componente básico que muestra puntajes organizados por categorías como Reacción,
+       Memoria, Verbal y Visual, con íconos representativos.`,
       technologies: ['React', 'TypeScript', 'TailwindCSS', 'Next JS'],
-      link: 'https://mentor-newbie-01-blog-preview-card.vercel.app/',
+      link: 'https://mentor-newbie-06-results-summary-component.vercel.app/',
       color: ''
     },
     {
@@ -71,10 +71,10 @@ export const frontendCards = [
       category: 'Newbie',
       image: '/ceo.jpg',
       title: 'Product preview card component',
-      description: `Una tarjeta HTML básica diseñada para mostrar vistas previas de blogs, 
-      incluyendo título, descripción breve e imagen destacada.`,
+      description: `Un componente básico que muestra un producto con imagen, nombre, 
+      descripción breve, precio y un botón de compra.`,
       technologies: ['React', 'TypeScript', 'TailwindCSS', 'Next JS'],
-      link: 'https://mentor-newbie-01-blog-preview-card.vercel.app/',
+      link: 'https://mentor-newbie-07-product-preview-card-component.vercel.app/',
       color: ''
     },
     {
@@ -82,10 +82,10 @@ export const frontendCards = [
       category: 'Newbie',
       image: '/ceo.jpg',
       title: 'Skilled e-learning landing page',
-      description: `Una tarjeta HTML básica diseñada para mostrar vistas previas de blogs, 
-      incluyendo título, descripción breve e imagen destacada.`,
+      description: `Landing básica para una plataforma de aprendizaje, destacando habilidades clave, 
+      beneficios del e-learning y opciones accesibles para usuarios.`,
       technologies: ['React', 'TypeScript', 'TailwindCSS', 'Next JS'],
-      link: 'https://mentor-newbie-01-blog-preview-card.vercel.app/',
+      link: 'https://mentor-newbie-08-skilled-e-learning-landing.vercel.app/',
       color: ''
     },
     {
@@ -93,10 +93,10 @@ export const frontendCards = [
       category: 'Newbie',
       image: '/ceo.jpg',
       title: 'Interactive rating component',
-      description: `Una tarjeta HTML básica diseñada para mostrar vistas previas de blogs, 
-      incluyendo título, descripción breve e imagen destacada.`,
+      description: `Un componente simple donde los usuarios califican del 1 
+      al 5 y ven el resultado final después de enviar`,
       technologies: ['React', 'TypeScript', 'TailwindCSS', 'Next JS'],
-      link: 'https://mentor-newbie-01-blog-preview-card.vercel.app/',
+      link: 'https://mentor-newbie-09-interactive-rating-component.vercel.app/',
       color: ''
     },
     {
@@ -104,10 +104,10 @@ export const frontendCards = [
       category: 'Newbie',
       image: '/ceo.jpg',
       title: 'QR code component',
-      description: `Una tarjeta HTML básica diseñada para mostrar vistas previas de blogs, 
-      incluyendo título, descripción breve e imagen destacada.`,
+      description: `Un componente simple que muestra un código QR generable
+       dinámicamente, permitiendo a los usuarios escanear y acceder a información o enlaces.`,
       technologies: ['React', 'TypeScript', 'TailwindCSS', 'Next JS'],
-      link: 'https://mentor-newbie-01-blog-preview-card.vercel.app/',
+      link: 'https://mentor-newbie-10-qr-code-component.vercel.app/',
       color: ''
     },
     {
@@ -115,10 +115,10 @@ export const frontendCards = [
       category: 'Newbie',
       image: '/ceo.jpg',
       title: 'NFT preview card component',
-      description: `Una tarjeta HTML básica diseñada para mostrar vistas previas de blogs, 
-      incluyendo título, descripción breve e imagen destacada.`,
+      description: `Un componente que muestra una vista previa de un NFT, 
+      con imagen, título, descripción breve y detalles como precio o propietario.`,
       technologies: ['React', 'TypeScript', 'TailwindCSS', 'Next JS'],
-      link: 'https://mentor-newbie-01-blog-preview-card.vercel.app/',
+      link: 'https://mentor-newbie-11-nft-preview-card-component.vercel.app/',
       color: ''
     },
     {
@@ -126,10 +126,10 @@ export const frontendCards = [
       category: 'Newbie',
       image: '/ceo.jpg',
       title: 'Equalizer landing page',
-      description: `Una tarjeta HTML básica diseñada para mostrar vistas previas de blogs, 
-      incluyendo título, descripción breve e imagen destacada.`,
+      description: `Landing page para una app de ecualizador de audio en Android e iOS, 
+      con control avanzado de graves, medios, agudos y más.`,
       technologies: ['React', 'TypeScript', 'TailwindCSS', 'Next JS'],
-      link: 'https://mentor-newbie-01-blog-preview-card.vercel.app/',
+      link: 'https://mentor-newbie-12-equalizer-landing-page.vercel.app/',
       color: ''
     },
     {
@@ -137,10 +137,10 @@ export const frontendCards = [
       category: 'Newbie',
       image: '/ceo.jpg',
       title: 'Order summary component',
-      description: `Una tarjeta HTML básica diseñada para mostrar vistas previas de blogs, 
-      incluyendo título, descripción breve e imagen destacada.`,
+      description: `Componente que muestra el resumen de un plan anual, 
+      incluyendo detalles del plan, duración, precio y total a pagar.`,
       technologies: ['React', 'TypeScript', 'TailwindCSS', 'Next JS'],
-      link: 'https://mentor-newbie-01-blog-preview-card.vercel.app/',
+      link: 'https://mentor-newbie-13-order-summary-component.vercel.app/',
       color: ''
     },
     {
@@ -148,10 +148,11 @@ export const frontendCards = [
       category: 'Newbie',
       image: '/ceo.jpg',
       title: 'Stats preview card component',
-      description: `Una tarjeta HTML básica diseñada para mostrar vistas previas de blogs, 
-      incluyendo título, descripción breve e imagen destacada.`,
-      technologies: ['React', 'TypeScript', 'TailwindCSS', 'Next JS'],
-      link: 'https://mentor-newbie-01-blog-preview-card.vercel.app/',
+      description: `Componente que muestra una tarjeta con estadísticas clave, destacando
+       beneficios de análisis de datos para mejorar decisiones sobre ingresos, experiencia del
+        cliente y eficiencia.`,
+      technologies: ['React', 'TypeScript', 'TailwindCSS', 'Next JS', 'GSAP'],
+      link: 'https://mentor-newbie-14-stats-preview-card-component.vercel.app/',
       color: ''
     },
     {
@@ -159,10 +160,10 @@ export const frontendCards = [
       category: 'Newbie',
       image: '/ceo.jpg',
       title: 'Meet landing page',
-      description: `Una tarjeta HTML básica diseñada para mostrar vistas previas de blogs, 
-      incluyendo título, descripción breve e imagen destacada.`,
+      description: `Landing para una app de chat grupal, facilitando la conexión cara a cara virtualmente
+       y la colaboración en cualquier dispositivo.`,
       technologies: ['React', 'TypeScript', 'TailwindCSS', 'Next JS'],
-      link: 'https://mentor-newbie-01-blog-preview-card.vercel.app/',
+      link: 'https://mentor-newbie-15-meet-landing-page.vercel.app/',
       color: ''
     },
   ];
